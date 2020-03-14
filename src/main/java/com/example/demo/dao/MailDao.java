acd5c4400abfe840225cf0ec3a5e16fee7bf569c
shiguang999 <44136021+shiguang999@users.noreply.github.com>
2020-03-14 16:48:47 +0800

    修改

--- src/main/java/com/example/demo/dao/MailDao.java
+++ src/main/java/com/example/demo/dao/MailDao.java
@@ -3,6 +3,7 @@ package com.example.demo.dao;
 import com.example.demo.entrty.Emil;
 import com.example.demo.entrty.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +11,7 @@ import java.util.List;
 @Mapper
 @Repository
 public interface MailDao {
-    List<User> queryUser(User user);
+    List<User> queryUser(@Param("user") User user);
 
     List<Emil> queryMail(Emil user);
 
